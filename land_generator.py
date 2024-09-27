bfd2a0ff3429014dc3fe49fec87e17dddef1382d
FlavioT04 <flavio.mendotri@gmail.com>
2024-09-27 01:21:07 -0230

    Changes to class LandGenerator

--- land_generator.py
+++ land_generator.py
@@ -1,21 +1,22 @@
 
 class LandGenerator:
     """
-    A class to represent a location (e.g., island or land mass) in the simulation.
+    A class to represent a landmass (e.g., island or land mass) in the simulation.
     Each location has a name and a position (x, y coordinates).
     """
 
-    def __init__(self, name, x, y):
+    def __init__(self, name, type, detector_coords):
         """
         Initialize a new location with a name and coordinates.
 
         Args:
         - name (str): The name of the location (e.g., Island G).
-        - x (float): The x-coordinate of the location on the map.
-        - y (float): The y-coordinate of the location on the map.
+        - type (str): The type of detector.
+        - detector_coords (list): list with x and y coordinates of the detectors.
         """
-        self.name = name    # Name of the location
-        self.position = (x, y)  # Coordinates of the location
+        self.name = name   
+        self.type = type 
+        self.coordinates = detector_coords
 
     def get_position(self):
         """
@@ -24,7 +25,7 @@ class LandGenerator:
         Returns:
         - tuple: The (x, y) coordinates of the location.
         """
-        return self.position
+        return self.coordinates
     
     def __str__(self):
         """
@@ -33,5 +34,5 @@ class LandGenerator:
         Returns:
         - str: A string describing the location and its position.
         """
-        return f"Location {self.name} at position {self.position}"
+        return f"{self.name} Type: {self.type}"
     
