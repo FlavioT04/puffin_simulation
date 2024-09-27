046411d141804b13cadfeb9f4aaaecce4f2e74fb
FlavioT04 <flavio.mendotri@gmail.com>
2024-09-27 01:21:42 -0230

    main.py updated

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from land_generator import LandGenerator
 
 def main():
     gull_island = LandGenerator("Gull Island", 0, 0)
+    witless_bay = LandGenerator("Witless Bay", -1, 0)
     print("source:", gull_island)
-    simulate_puffin_movement()
+    simulate_puffin_movement(gull_island, witless_bay)
 
 if __name__ == "__main__":
     main()
