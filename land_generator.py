7cb68f2d613d57971f0cdc0a7964fb8188e9ae3f
FlavioT04 <flavio.mendotri@gmail.com>
2024-09-29 18:48:18 -0230

    method get_type() added to the class

--- land_generator.py
+++ land_generator.py
@@ -11,21 +11,30 @@ class LandGenerator:
 
         Args:
         - name (str): The name of the location (e.g., Island G).
-        - type (str): The type of detector.
-        - detector_coords (list): list with x and y coordinates of the detectors.
+        - type (str): The type of detector (emitter or receiver).
+        - detector_coords (list): list consisting of tuples (x, y) that represent the coordinates.
         """
         self.name = name   
         self.type = type 
         self.coordinates = detector_coords
 
-    def get_position(self):
+    def get_coordinates(self):
         """
         Get the coordinates of the location.
 
         Returns:
-        - tuple: The (x, y) coordinates of the location.
+        - list: List containing coordinates (x, y).
         """
         return self.coordinates
+    
+    def get_type(self):
+        """
+        Get the type of detector
+
+        Returns:
+        - String; type of detector ('emitter' or 'receiver')
+        """
+        return self.type
     
     def __str__(self):
         """
@@ -34,5 +43,5 @@ class LandGenerator:
         Returns:
         - str: A string describing the location and its position.
         """
-        return f"{self.name} Type: {self.type}"
+        return f"{self.name}"
     
