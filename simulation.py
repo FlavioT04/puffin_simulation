c2a3de57aa931c131aaa78d2250bd30880b1849a
FlavioT04 <flavio.mendotri@gmail.com>
2025-04-11 22:12:24 -0230

    change made

--- simulation.py
+++ simulation.py
@@ -19,18 +19,10 @@ Args:
 Returns:
     - gdf (geopandas.GeoDataFrame): GeoDataFrame containing data about the puffins
 '''
-def simulation(start, destination, num_puffins, crs):
+def simulation(start, destination, num_puffins, bounce_option, bounce_val, max_elev, elev_val, probabilities_option, crs):
 
     # files needed
     witless_bay_grid = gpd.read_file('files/Grid_puffmap_2km/Grid_puffmap_2km.shp').to_crs(epsg=crs)
-
-    # settings
-    b_height = 6
-    b_angle = np.pi/4
-
-    # modes
-    bouncing_angle = False
-    bouncing_wall = True
 
     # spawn puffins and store them in a array
     puffins = [spawn_puffin(start, crs) for i in range(num_puffins)]
@@ -78,51 +70,54 @@ def simulation(start, destination, num_puffins, crs):
                 # update vector
                 puffin.set_vector(puffin_vector)
 
-                # when bouncing_wall is True, puffin bounces depending on the elevation of the intersecting geometry
-                if bouncing_wall == True:
+                # bouncing angle mode
+                if bounce_option == 1:
+                    # clip geometry
+                    coastline_vector = clip_geometry(destination, puffin_vector.intersection(destination))
+                    # get smallest angle
+                    angle = get_intersecting_angle(puffin_vector, coastline_vector)
+                    if angle > np.pi/2:
+                        angle = np.pi - angle
+                    bounces = 20
+                    bouncing = True
+                    while bouncing and bounces > 0:
+                        # check for bouncing condition
+                        if angle <= bounce_val:
+                            bounces -= 1
+                            bounce_along_coastline(puffin, angle, destination)
+                            puffin_moving = True
+                            while puffin_moving:
+                                puffin.move()
+                                if puffin.get_vector().intersects(destination).any():
+                                    coastline_vector = clip_geometry(destination, puffin.get_vector().intersection(destination))
+                                    angle = get_intersecting_angle(puffin.get_vector(), coastline_vector)
+                                    puffin_moving = False
+                                if puffin.get_steps() < 1:
+                                    puffin_moving = False
+                        else:
+                            bouncing = False
+                    print(np.rad2deg(angle))
                     
+                # when bouncing_wall is True, puffin bounces depending on the elevation of the intersecting geometry
+                if max_elev == 1:
                     # get the elevation of the cell
-                    cell_elevation = intersecting_cell(witless_bay_grid, puffin_vector)['MeanElevat']
-
-                    # check conditions
-                    if cell_elevation <= b_height:
-                        print('------Puffin moves inland------')
-                        print('cell elevation:', cell_elevation)
-                        puffin.move()
-                        while is_moving:
-                            # get random integer from 0 to 100 (exclusive)
-                            random_num = np.random.randint(0, 100)
-                            print(random_num)        
-                            if cell_elevation >= 0 and cell_elevation <= 2:
-                                if random_num < 50:
-                                    is_moving = False
-                                else: 
-                                    for i in range(20):
-                                        puffin_vector = puffin.move()
-                            if cell_elevation > 2 and cell_elevation <= 4:
-                                if random_num < 75:
-                                    is_moving = False
-                                else: 
-                                    for i in range(20):
-                                        puffin_vector = puffin.move()
-                            if cell_elevation > 4:
-                                is_moving = False
-                            if cell_elevation < 0:
-                                print('elevation is negative')
-                                is_moving = False
-                            
-                            if is_moving:
-                                cell = intersecting_cell(witless_bay_grid, puffin_vector)
-                                if cell['cell_ID'] == 'None':
-                                    print('new cell elevation:', None)
-                                    is_moving = False
-                                else:
-                                    cell_elevation = cell['MeanElevat']
-                                print('new cell elevation:', cell_elevation)
+                    print(intersecting_cell(witless_bay_grid, puffin_vector))
+                    cell_elevation = intersecting_cell(witless_bay_grid, puffin_vector)
+                    if cell_elevation['cell_ID'] == 'None':
+                        print('puffin is out of bounds')
                     else:
-                        print('--------puffin bounces--------')
-                        print('cell elevation:', cell_elevation)
-        
+                        cell_elevation = cell_elevation['MeanElevat']
+                        # check conditions
+                        if cell_elevation <= elev_val:
+                            print('Puffin stops')
+                        else:
+                            print('--------puffin bounces--------')
+                            print('cell elevation:', cell_elevation)
+
+                # probabilities mode
+                if probabilities_option == 1:
+                    go_inland(witless_bay_grid, puffin)
+                
                 # stop puffin
                 is_moving = False
 
@@ -248,7 +243,8 @@ def get_intersecting_angle(v, u):
     magu = np.sqrt(un[0]**2 + un[1]**2)
 
     # compute angle
-    angle = np.arccos(np.dot(vn, un) / (magv * magu))
+    cos_angle = np.clip(np.dot(vn, un) / (magv * magu), -1.0, 1.0)
+    angle = np.arccos(cos_angle)
 
     return angle
 
@@ -263,6 +259,10 @@ Returns:
     - clipped (geopandas.GeoSeries): clipped geometry
 '''
 def clip_geometry(geometry, point):
+
+    if isinstance(point.iloc[0], shapely.MultiPoint):
+        multipoint = point.iloc[0]
+        point = gpd.GeoSeries([list(multipoint.geoms)[0]])
 
     x = point.x.iloc[0]
     y = point.y.iloc[0]
@@ -285,6 +285,7 @@ Returns:
     - cell (geopandas.GeoSeries): cell containing the given puffin
 '''
 def intersecting_cell(grid, puffin_vector):
+
     searching = True
     cell = gpd.GeoDataFrame({'cell_ID': ['None'], 'geometry': Point(0, 0)}, crs=grid.crs).iloc[0]
     i = 0
@@ -295,4 +296,86 @@ def intersecting_cell(grid, puffin_vector):
             searching = False
         else:
             i += 1
-    return cell        
+    return cell
+
+'''
+bouncing_wall mode 
+'''
+def go_inland(grid, puffin):
+
+    puffin.move()
+
+    # get the elevation of the cell
+    cell_elevation = intersecting_cell(grid, puffin.get_vector())
+    if cell_elevation['cell_ID'] == 'None':
+        print('puffin is out of bounds')
+    else:
+        cell_elevation = cell_elevation['MeanElevat']
+        cell_id = intersecting_cell(grid, puffin.get_vector())['cell_ID']
+
+        print('------Puffin moves inland------')
+        print('cell elevation:', cell_elevation, cell_id)
+        is_moving = True
+        while is_moving:
+
+            # get random integer from 0 to 100 (exclusive)
+            random_num = np.random.randint(0, 100)
+            print(random_num)
+
+            # check conditions
+            if cell_elevation >= 0 and cell_elevation <= 2:
+                if random_num < 50:
+                    is_moving = False
+                else:
+                    for _ in range(15):
+                        puffin.move()
+
+            if cell_elevation > 2 and cell_elevation <= 4:
+                if random_num < 75:
+                    is_moving = False
+                else:
+                    for _ in range(15):
+                        puffin.move()
+
+            if cell_elevation > 4 or cell_elevation < 0:
+                print('Elevation greater than 4 or less than 0', cell_elevation)
+                is_moving = False
+
+            # get new elevation if puffin still moving
+            if is_moving:
+                cell = intersecting_cell(grid, puffin.get_vector())
+                cell_id = intersecting_cell(grid, puffin.get_vector())['cell_ID']
+                if cell['cell_ID'] == 'None':
+                    print('new cell elevation:', None)
+                    is_moving = False
+                else:
+                    cell_elevation = cell['MeanElevat']
+                print('new cell elevation:', cell_elevation, cell_id)
+
+def bounce_along_coastline(puffin, angle, destination):
+
+    x = puffin.get_position()[0]
+    y = puffin.get_position()[1]
+    if puffin.get_direction() < np.pi:
+        print('Rotating anticlockwise')
+        print('direction:', np.rad2deg(puffin.get_direction()))
+        new_vector = puffin.get_vector().rotate(angle=(angle), use_radians=True, origin=(x,y))
+        new_direction = puffin.get_direction() + angle
+        if new_vector.intersects(destination).any():
+            new_vector = puffin.get_vector().rotate(angle=-(angle), use_radians=True, origin=(x,y))
+            new_direction = puffin.get_direction() - angle
+            print('check new angle!! (anticlockwise condition)')
+    else:
+        print('Rotating clockwise')
+        print('direction:', np.rad2deg(puffin.get_direction()))
+        new_vector = puffin.get_vector().rotate(angle=-(angle), use_radians=True, origin=(x,y))
+        new_direction = puffin.get_direction() - angle
+        if new_vector.intersects(destination).any():
+            new_vector = puffin.get_vector().rotate(angle=(angle), use_radians=True, origin=(x,y))
+            new_direction = puffin.get_direction() + angle
+            print('check new angle!! (clockwise condition)')
+    puffin.set_vector(new_vector)
+    puffin.set_direction(new_direction)
+    print('new direction:', np.rad2deg(new_direction))
+    print('position:', x, y)
+
